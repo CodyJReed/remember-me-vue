2e3482bc1496cb08300b3c9c9430f5c01ef82ce4
CodyJReed <cody2j2reed@gmail.com>
2021-08-30 14:14:30 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <the-header title="RememberMe" />
+  <stored-resources :resources="storedResources" />
+</template>
+
+<script>
+import StoredResources from './components/resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+export default {
+  components: {
+    StoredResources,
+    TheHeader
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Offical Guide',
+          desc: 'offical vue js docs',
+          link: 'vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'google',
+          desc: 'learn to google...',
+          link: 'google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+h3 {
+  text-transform: capitalize;
+}
+</style>
---
+++ src/components/resources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="item in resources"
+      :key="item.id"
+      :resource="item"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+  components: {
+    LearningResource
+  },
+  props: ['resources'],
+  mounted() {
+    console.log(this.resources);
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
